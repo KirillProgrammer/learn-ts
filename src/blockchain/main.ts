95ad84bbf19e04210258299fa60c7cc5796053f5
KirillProgrammer <kirillkarpovprogramm@gmail.com>
2022-05-17 08:00:58 +0300

    начинаем делать мини блокчейн и разбираться как он работает

---
+++ src/blockchain/main.ts
@@ -0,0 +1,12 @@
+import { Blockchain } from "./blockchain"
+
+console.log('Creating blockchain with genesis block...')
+const blockchain = new Blockchain()
+
+console.log('Mining block #1...')
+blockchain.addBlock('First block')
+
+console.log('Mining block #2...')
+blockchain.addBlock('Second block')
+
+console.log(JSON.stringify(blockchain, null, 2))
